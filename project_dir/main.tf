ab94478e9c939fb5cd908ecc815442dad2c96c8e
shafi <shafisnpni@gmail.com>
2022-09-15 17:11:36 +0530

    made change in alignment

--- project_dir/main.tf
+++ project_dir/main.tf
@@ -1,10 +1,10 @@
 module "alb" {
 
-    source = "../modules/alb"
-    env = var.env
-    project = var.project
-    sg = var.sg
-    domain = var.domain
+  source = "../modules/alb"
+  env = var.env
+  project = var.project
+  sg = var.sg
+  domain = var.domain
 }
 
 module "asg" {
@@ -26,93 +26,94 @@ module "asg" {
 
 resource "aws_codedeploy_app" "node_app" {
 
-    name             = "node_app"
-    compute_platform = "Server"
-    tags             = {
-      env = var.env
+  name             = "node_app"
+  compute_platform = "Server"
+  tags             = {
+  env = var.env
     }
 }
 
 
 resource "aws_codedeploy_deployment_group" "deployment_group" {
 
-    app_name               = aws_codedeploy_app.node_app.name
-    autoscaling_groups     = [ module.asg.asg_op.name ]
-    deployment_config_name = "CodeDeployDefault.HalfAtATime"
-    deployment_group_name  = "demo-code-deployment"
-    service_role_arn       = data.aws_iam_role.deployment_role.arn
-    tags                   = {}
+  app_name               = aws_codedeploy_app.node_app.name
+  autoscaling_groups     = [ module.asg.asg_op.name ]
+  deployment_config_name = "CodeDeployDefault.HalfAtATime"
+  deployment_group_name  = "demo-code-deployment"
+  service_role_arn       = data.aws_iam_role.deployment_role.arn
+  tags                   = {}
 
-    auto_rollback_configuration {
-        enabled = true
-        events  = [ "DEPLOYMENT_FAILURE" ]
-    }
+  auto_rollback_configuration {
+    
+    enabled = true
+    events  = [ "DEPLOYMENT_FAILURE" ]
+  }
 
-    deployment_style {
-        deployment_option = "WITH_TRAFFIC_CONTROL"
-        deployment_type   = "IN_PLACE"
-    }
+  deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "IN_PLACE"
+  }
 
-    load_balancer_info {
+  load_balancer_info {
 
-        target_group_info {
-            name = module.alb.traget_group.name
+    target_group_info {
+      name = module.alb.traget_group.name
         }
     }
 }
 
 resource "aws_codepipeline" "codepipeline" {
 
-    name     = "demo"
-    role_arn = data.aws_iam_role.pipeline.arn
-    tags     = {}
+  name     = "demo"
+  role_arn = data.aws_iam_role.pipeline.arn
+  tags     = {}
   
 
-    artifact_store {
-        location = "artifact-for-codedeploy"
-        type     = "S3"
-    }
+  artifact_store {
+    location = "artifact-for-codedeploy"
+    type     = "S3"
+  }
 
-    stage {
-        name = "Source"
+  stage {
+    name = "Source"
 
-        action {
-            category         = "Source"
-            configuration    = {
-                "PollForSourceChanges" = "false"
-                "S3Bucket"             = "artifact-for-codedeploy"
-                "S3ObjectKey"          = "SampleWebApp.zip"
-            }
-            input_artifacts  = []
-            name             = "Source"
-            namespace        = "SourceVariables"
-            output_artifacts = [ "SourceArtifact" ]
-            owner            = "AWS"
-            provider         = "S3"
-            region           = "ap-south-1"
-            run_order        = 1
-            version          = "1"
+    action {
+      category         = "Source"
+      configuration    = {
+      "PollForSourceChanges" = "false"
+      "S3Bucket"             = "artifact-for-codedeploy"
+      "S3ObjectKey"          = "SampleWebApp.zip"
         }
-    }
-    stage {
-        name = "Deploy"
-
-        action {
-            category         = "Deploy"
-            configuration    = {
-                "ApplicationName"     = aws_codedeploy_app.node_app.name
-                "DeploymentGroupName" = aws_codedeploy_deployment_group.deployment_group.deployment_group_name
-            }
-            input_artifacts  = [ "SourceArtifact" ]
-            name             = "Deploy"
-            namespace        = "DeployVariables"
-            output_artifacts = []
-            owner            = "AWS"
-            provider         = "CodeDeploy"
-            region           = "ap-south-1"
-            run_order        = 1
-            version          = "1"
+      input_artifacts  = []
+      name             = "Source"
+      namespace        = "SourceVariables"
+      output_artifacts = [ "SourceArtifact" ]
+      owner            = "AWS"
+      provider         = "S3"
+      region           = "ap-south-1"
+      run_order        = 1
+      version          = "1"
         }
+  }
+    stage {
+      name = "Deploy"
+
+      action {
+      category         = "Deploy"
+      configuration    = {
+      "ApplicationName"     = aws_codedeploy_app.node_app.name
+      "DeploymentGroupName" = aws_codedeploy_deployment_group.deployment_group.deployment_group_name
+      }
+      input_artifacts  = [ "SourceArtifact" ]
+      name             = "Deploy"
+      namespace        = "DeployVariables"
+      output_artifacts = []
+      owner            = "AWS"
+      provider         = "CodeDeploy"
+      region           = "ap-south-1"
+      run_order        = 1
+      version          = "1"
+      }
     }
     depends_on = [
       aws_codedeploy_deployment_group.deployment_group
