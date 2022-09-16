1343e12b2525393c352c654d93aaea8ab0f4ad26
shafi <shafisnpni@gmail.com>
2022-09-16 11:08:56 +0530

    removed unnecessary lines

--- project_dir/datasource.tf
+++ project_dir/datasource.tf
@@ -1,17 +1,3 @@
-data "aws_iam_role" "deployment_role" {
-
-  name = "CodeDeployRole"
-}
-
-data "aws_iam_policy" "deployment_policy" {
-  name = "AWSCodeDeployRole"
-}
-
-data "aws_iam_role" "pipeline" {
-
-  name = "AWSCodePipelineServiceRole-ap-south-1-nodejsapp-pipeline"
-}
-
 data "aws_iam_policy" "codedeploy_managed_policy" {
 
   name = "AWSCodeDeployRole"
